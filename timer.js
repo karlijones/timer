7e9ede1593bdddc3ab1a6bd631b15a4d3d359315
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-05 22:35:06 -0500

    Adding more timer annimation code

--- timer.js
+++ timer.js
@@ -17,7 +17,7 @@ class Timer {
 // calling this.tick manually here will prevent any delay when the user clicks the timer
     start = () => {
         if (this.onStart) {
-          this.onStart();  
+          this.onStart(this.timeRemaining);  
         }
         this.tick();
         this.interval = setInterval(this.tick, 50);
@@ -34,9 +34,9 @@ class Timer {
                 this.onComplete();
             }
         } else {
-            this.timeRemaining = timeRemaining - .05;
+            this.timeRemaining = timeRemaining - 0.05;
             if (this.onTick) {
-                this.onTick();
+                this.onTick(this.timeRemaining);
             }
         }
     };
