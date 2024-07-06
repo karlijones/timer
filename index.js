f4a7604d55a9408260fc0826da4cc21ef32e6c3a
Karli Jones <130190464+karlijones@users.noreply.github.com>
2024-07-05 22:09:56 -0500

    Timer details added

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
 
 const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
+let currentOffset = 0;
 const timer = newTimer(durationInput, startButton, pauseButton {
     onStart() {
         console.log('Timer started');
     },
     onTick() {
-        console.log('Timer just ticked down');
+        circle.setAttribute('stroke-dashoffset', currentOffset);
+        currentOffset = currentOffset - 50;
     },
     onComplete() {
         console.log('Timer is completed');
